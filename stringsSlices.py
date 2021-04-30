bca233991b88467380d4c40e7928390241463a56
yurippo <83097615+yurippo@users.noreply.github.com>
2021-04-30 01:54:43 -0700

    Add files via upload
    python slices

---
+++ stringsSlices.py
@@ -0,0 +1,29 @@
+#####Python slices are used to slice a string living the first string examples below
+
+string1 =("O E2 10.110.8.9 [160/5] via 10.119.254.6, 0:01:00, Ethernet2")
+
+print (string1)
+
+print (string1 [5:15])
+
+print (string1 [5:14])
+
+print (string1 [5:])
+
+print (string1 [:10])
+
+print (string1 [:])
+
+print (string1 [-1])
+
+print (string1 [-2])
+
+print (string1 [-9:-1])
+
+print (string1 [-5:])
+
+print (string1 [:-5])
+
+print (string1 [::2])
+
+print (string1 [::-1])
