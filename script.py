e50ff33ca55f920d1e2188588bc766fafbcf596d
yurippo <83097615+yurippo@users.noreply.github.com>
2021-05-01 02:14:42 -0700

    Create script.py

---
+++ script.py
@@ -0,0 +1,11 @@
+####Hello World and Welcome
+
+my_name = "Yuri"
+print("Hello and welcome " + my_name + "!")
+
+###I want to write a Python program
+###that returns my name
+
+print("Hello world!")
+
+
