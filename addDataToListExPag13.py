76a8666e44fb9e46b072f528de26eb3e62846774
yurippo <83097615+yurippo@users.noreply.github.com>
2021-05-01 23:40:28 -0700

    Create addDataToListExPag13.py

---
+++ addDataToListExPag13.py
@@ -0,0 +1,23 @@
+##Working with python list adding exercise page 13
+
+movies = ["The Holy Grail","The Life of Bryan","The Meaning of Life"]
+
+print (movies)
+
+movies.insert(1,1975)
+
+print (movies)
+
+movies.insert(3,1979)
+
+print (movies)
+
+movies.insert(5,1983)
+
+print (movies)
+
+######code to re write the entire list all in one
+
+movies = ['The Holy Grail', 1975, 'The Life of Bryan', 1979, 'The Meaning of Life', 1983]
+
+print (movies)
