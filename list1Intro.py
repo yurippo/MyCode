73ef4797da568101b03de6bf8ee9fd277a9ebcf9
yurippo <83097615+yurippo@users.noreply.github.com>
2021-05-03 01:12:23 -0700

    Create list1Intro.py

---
+++ list1Intro.py
@@ -0,0 +1,22 @@
+#### We are creating a new list in Python
+
+list1 = []
+
+type(list1)
+
+List1 = ["Cisco", "Juniper", "Avaya", 10, 10.5, -11]
+
+len(list1)
+
+list1[0]
+
+list1[1]
+
+list1[-1]
+
+list1[-2]
+
+list1[100] ##that will return an index error
+
+list1[2] ="HP"
+
