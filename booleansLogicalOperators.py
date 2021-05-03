5c46c46a768ec32344155942983da418a90b7464
yurippo <83097615+yurippo@users.noreply.github.com>
2021-05-03 01:00:19 -0700

    Create booleansLogicalOperators.py

---
+++ booleansLogicalOperators.py
@@ -0,0 +1,18 @@
+    #Booleans - logical operations
+    (1 == 1) and (2 == 2) #result is True; AND means that both operands should be True in order to get the expression evaluated as True
+     
+    (1 == 1) or (2 == 2) #result is True; when using OR, it is enough if only one expression is True, in order to have True as the final result
+     
+    not(1 == 1) #result is False; using the NOT operator means denying an expression, in this case denying a True expression
+     
+    not(1 == 2) #result is True; using the NOT operator means denying an expression, in this case denying a False expression
+     
+    None, 0, 0.0, 0L, 0j, empty string, empty list, empty tuple, empty dictionary #these values always evaluate to False
+     
+    bool(None) #returns False; function that evaluates values and expressions
+     
+    bool(0) #returns False; function that evaluates values and expressions
+     
+    bool(2) #returns True; function that evaluates values and expressions
+     
+    bool("router") #returns True; function that evaluates values and expressions
