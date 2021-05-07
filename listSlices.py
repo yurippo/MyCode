c5028a3f7858bb74727117cc3a88a6b3bb11c034
yurippo <83097615+yurippo@users.noreply.github.com>
2021-05-07 02:54:16 -0700

    Create listSlices.py

---
+++ listSlices.py
@@ -0,0 +1,30 @@
+#####Please find below many example of lists slices in python 
+
+list3 = [1,2,3,"a","b","c"]
+
+list3
+
+list3[0:3]
+
+list3[:3]
+
+list3[2:5]
+
+
+list3[2:]
+
+list3[:]
+
+list3[-1]
+
+list3[-2]
+
+list3[-4:-1]
+
+list3[-3:]
+
+list3[:-3]
+
+list3[::2]
+
+list3[::-1]
