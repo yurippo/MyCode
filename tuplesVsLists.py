8c8132c6bbbe4d01feb0e6eac6bd05fd4ffe112c
yurippo <83097615+yurippo@users.noreply.github.com>
2021-05-14 03:18:05 -0700

    Add files via upload

---
+++ tuplesVsLists.py
@@ -0,0 +1,16 @@
+###tuples vs. lists
+
+#tuples are imutable tuples ahe a fixed lenght
+#lists are mutable and lists are a variable lenght
+
+#tuples use parantheses ()
+
+#Lists use square braquets []
+
+a = ()
+
+b = []
+
+type(a)
+
+type(b)
