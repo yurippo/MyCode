c84b547ae12dc28bc9ced6543ee8ce4426d78a16
yurippo <83097615+yurippo@users.noreply.github.com>
2021-05-15 03:36:01 -0700

    Add files via upload

---
+++ oddOrEven.py
@@ -0,0 +1,14 @@
+###Don't change the code below
+
+number = int(input("Which number do you want to check if it's Odd or Even? "))
+
+###Don't change the code above
+
+#####write your code below this line
+
+if number % 2 == 0:
+    print (f"The number {number} is Even")
+else:
+    print (f"The number {number} is Odd")
+
+####Yaheee done Code working :)    
