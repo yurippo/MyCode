b8cb1c65b1bbe36c4bb5856595db70658e9d5140
yurippo <83097615+yurippo@users.noreply.github.com>
2021-05-15 04:06:58 -0700

    Add files via upload

--- rollerCoaster.py
+++ rollerCoaster.py
@@ -4,5 +4,12 @@ height = float(input("What is your height in cm? "))
 
 if height > 120.0:
     print("You can ride the rollercoaster!")
+    age = int(input("What's your age? "))
+    if age < 12:
+        print("Please pay $5")
+    elif age <= 18:
+        print("Please pay $7")
+    else:
+        print("Please pay $12")
 else:
     print("Sorry, you have to grow taller before you can ride :(")
