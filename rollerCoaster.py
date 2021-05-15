f15fbad9c7315d5485952bf3a45e95ab67dc1d9e
yurippo <83097615+yurippo@users.noreply.github.com>
2021-05-15 03:06:20 -0700

    Add files via upload

---
+++ rollerCoaster.py
@@ -0,0 +1,8 @@
+print("Welcome to the rollercoaster!")
+
+height = float(input("What is your height in cm? "))
+
+if height > 120.0:
+    print("You can ride the rollercoaster!")
+else:
+    print("Sorry, you have to grow taller before you can ride :(")
