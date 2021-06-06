5beb64b5c067c9e991c2af25482be52e46a35c97
yurippo <83097615+yurippo@users.noreply.github.com>
2021-06-06 01:40:29 -0700

    Add files via upload

---
+++ randomExercise.py
@@ -0,0 +1,11 @@
+##Write your code below this line
+#Hint: Remember to import the
+#random module first.
+import random
+
+randomSide = random.randint(0, 1)
+
+if randomSide == 1:
+    print("Heads")
+else:
+    print("Tails")    
