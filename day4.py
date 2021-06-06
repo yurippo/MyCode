4ca1d7c485579dbbe628df8d3dcc60b36e373f4e
yurippo <83097615+yurippo@users.noreply.github.com>
2021-06-06 01:39:26 -0700

    Add files via upload

---
+++ day4.py
@@ -0,0 +1,13 @@
+import random
+
+randomInteger = random.randint(1, 10)
+print(randomInteger)
+
+#0.00000000 - 0.999999...
+
+randomFloat = random.random()
+print(randomFloat)
+
+randomFloat * 5
+
+#0.00000.... - 4.9999999....
